8efb097812a99306069ba3704758e21a413a0bf2
Ansgar Sachs <ansgar.sa@googlemail.com>
2020-02-23 18:09:28 +0100

    feat: add skeleton monolithic app

---
+++ patient-monolith/src/main/java/de/ansgarsachs/patientmonolith/PatientMonolithApplication.java
@@ -0,0 +1,13 @@
+package de.ansgarsachs.patientmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientMonolithApplication.class, args);
+    }
+
+}
