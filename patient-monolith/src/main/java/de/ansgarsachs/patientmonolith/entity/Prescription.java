eb69930d40ec247690ad71361ef897639d3ac833
Ansgar Sachs <ansgar.sa@googlemail.com>
2020-02-23 20:14:20 +0100

    fix: properly create json objects

---
+++ patient-monolith/src/main/java/de/ansgarsachs/patientmonolith/entity/Prescription.java
@@ -0,0 +1,14 @@
+package de.ansgarsachs.patientmonolith.entity;
+
+/**
+ * Prescription
+ *
+ * <p>
+ * TODO: Write a description
+ * </p>
+ *
+ * @author Ansgar Sachs &lt;ansgar.sachs@cgm.com&gt;
+ * @since 23.02.20
+ */
+public class Prescription {
+}
