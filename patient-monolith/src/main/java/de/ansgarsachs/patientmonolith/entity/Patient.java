ed7d65f5b46dabdb57f7016a930ad3268f32bda5
Ansgar Sachs <ansgar.sa@googlemail.com>
2020-02-23 20:16:14 +0100

    feat: add spring monolith

--- patient-monolith/src/main/java/de/ansgarsachs/patientmonolith/entity/Patient.java
+++ patient-monolith/src/main/java/de/ansgarsachs/patientmonolith/entity/Patient.java
@@ -3,12 +3,54 @@ package de.ansgarsachs.patientmonolith.entity;
 /**
  * Patient
  *
- * <p>
- * TODO: Write a description
- * </p>
- *
  * @author Ansgar Sachs &lt;ansgar.sachs@cgm.com&gt;
  * @since 23.02.20
  */
 public class Patient {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String toString() {
+        return String.format(
+                "Patient{id='%s', firstName='%s', lastName='%s', age='%s'}",
+                id,
+                firstName,
+                lastName,
+                age
+        );
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
