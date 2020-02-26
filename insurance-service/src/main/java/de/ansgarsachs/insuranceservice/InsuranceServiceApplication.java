b35904e3c16edc497cb7d4bd3a741cbca9dd38bb
Ansgar Sachs <ansgar.sa@googlemail.com>
2020-02-26 17:21:12 +0100

    feat: add patient and insurance service

---
+++ insurance-service/src/main/java/de/ansgarsachs/insuranceservice/InsuranceServiceApplication.java
@@ -0,0 +1,13 @@
+package de.ansgarsachs.insuranceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceServiceApplication.class, args);
+    }
+
+}
---
+++ patient-service/src/main/java/de/ansgarsachs/patientservice/PatientServiceApplication.java
@@ -0,0 +1,13 @@
+package de.ansgarsachs.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
+
+}
