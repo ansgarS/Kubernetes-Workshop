8addeff05500edafec150038e1ea1de9ca46f990
Ansgar Sachs <ansgar.sa@googlemail.com>
2020-02-29 11:23:17 +0100

    feat: initial insurance service implementation

---
+++ insurance-service/src/main/java/de/ansgarsachs/insuranceservice/control/PatientRegistry.java
@@ -0,0 +1,18 @@
+package de.ansgarsachs.insuranceservice.control;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * PatientRegistry
+ *
+ * @author Ansgar Sachs &lt;ansgar.sachs@cgm.com&gt;
+ * @since 27.02.20
+ */
+@Service
+public class PatientRegistry  {
+    private static final Logger log = LoggerFactory.getLogger(PatientRegistry.class);
+
+
+}
